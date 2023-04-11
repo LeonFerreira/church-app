8d3f4ad29e183051b9e9fb755e688054b9455d9b
Leonardo Ferreira <ferreira.sleonardo@gmail.com>
2023-04-11 16:23:15 -0300

    Outro

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -22,20 +22,20 @@
     <input type="password" name="password" placeholder="Password">
     <input type="tel" name="phone" placeholder="Phone" value="{{ $user->phone }}">
     <label for="birthday">Birthday</label>
-    <input type="date" name="birthday" placeholder="Brithday" value="{{ old('birthday') }}">
-    <input type="text" name="address" placeholder="Address" value="{{ old('address') }}">
-    <input type="number" name="address_number" min="0" placeholder="Address Number" value="{{ old('address_number') }}">
-    <input type="text" name="complement" placeholder="Complement" value="{{ old('complement') }}">
+    <input type="date" name="birthday" placeholder="Brithday" value="{{ $user->birthday }}">
+    <input type="text" name="address" placeholder="Address" value="{{ $user->address }}">
+    <input type="number" name="address_number" min="0" placeholder="Address Number" value="{{ $user->address_number ?? old('address_number') }}">
+    <input type="text" name="complement" placeholder="Complement" value="{{ $user->complement ?? old('complement') }}">
     <label for="baptized">Baptized</label>
-    <input type="date" name="baptized" placeholder="Baptized" value="{{ old('baptized') }}">
-    <select name="gender" value="{{ old('gender') }}">
+    <input type="date" name="baptized" placeholder="Baptized" value="{{ $user->baptized ?? old('baptized') }}">
+    <select name="gender">
         @foreach(array_column(\App\Enums\Gender::cases(), 'value') as $option)
-            <option value="{{$option}}">{{$option}}</option>
+            <option @if($option == $user->gender) selected @endif value="{{$option}}">{{$option}}</option>
         @endforeach
     </select>
-    <select name="marital_status" value="{{ old('marital_status') }}">
+    <select name="marital_status">
         @foreach(array_column(\App\Enums\MaritalStatus::cases(), 'value') as $option)
-            <option value="{{$option}}">{{$option}}</option>
+            <option @if($option == $user->marital_status) selected @endif value="{{$option}}">{{$option}}</option>
         @endforeach
     </select>
     <button type="submit">Send</button>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/church', [ChurchController::class, 'index'])->name('churchs.index');
 
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
